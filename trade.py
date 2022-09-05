e006b34d099e5ac7de03fa2c58db381d89c3c3ac
Hyungjun Lim <sculd3@gmail.com>
2022-09-05 12:49:47 -0400

    sandbox_market_data

--- trade.py
+++ trade.py
@@ -11,26 +11,6 @@ from rl.agents.dqn import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
-
-
-from trading.envs.trading_env import MarketData, TrainTestDataType
-md = MarketData('data.csv')
-for entry in md:
-    print(entry)
-
-print('shuffle and re iterate')
-md.reset()
-for entry in md:
-    print(entry)
-
-md.set_train_test(TrainTestDataType.TEST)
-for entry in md:
-    print(entry)
-
-md.reset()
-for entry in md:
-    print(entry)
-
 from trading.envs.trading_env import TradingEnv, TradeSideType, TradingEnvInitParam
 
 filename = 'data.csv'
